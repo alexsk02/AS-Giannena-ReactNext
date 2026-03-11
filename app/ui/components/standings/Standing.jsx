432fecf8df3fac3ebf7685d2eb4d87a8c8dd0a11
Alex <alexskouras02@gmail.com>
2026-03-11 13:57:17 +0200

    Girls U18 Standings Update

--- app/ui/components/standings/Standing.jsx
+++ app/ui/components/standings/Standing.jsx
@@ -2,7 +2,19 @@ import "@/app/ui/styles/standings/Standing.css";
 import { Fetch_Standings } from "@/app/ui/lib/utils";
 
 export default async function Standing({ team }) {
-  const teams = await Fetch_Standings(team);
+  let teams = await Fetch_Standings(team);
+
+  if (team === "girls-u18-teams") {
+    const allowedTeams = [
+      "ΑΣ Γιάννενα",
+      "ΠΑΣ Γιάννινα",
+      "ΑΓΟ Φιλιππιάδας",
+      "ΑΟ Φιλία Άρτας",
+      "ΚΑΟ Αχιλλέας",
+    ];
+
+    teams = teams.filter((t) => allowedTeams.includes(t.name));
+  }
 
   if (!teams || teams.length === 0) {
     return (
